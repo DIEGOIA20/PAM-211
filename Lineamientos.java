d9dff452b53cc4709ea2748550445f36b4c9e3f0
DIEGOIA20 <124050360@upq.edu.mx>
2025-09-04 21:35:11 -0600

    Primera parte

---
+++ Lineamientos.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+
+// Clase principal con los métodos
+class InicioPAM {
+private String reglamentoPOO;
+private String lineamientosClassroom;
+private String fechasParciales;
+private String porcentajesParcial;
