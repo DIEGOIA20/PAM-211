5cdcf19d6aba84711d42ea096ef58462c6cbc90f
DIEGOIA20 <124050360@upq.edu.mx>
2025-09-25 20:31:24 -0600

    Ejercicio A

---
+++ Practica3/main.js
@@ -0,0 +1,6 @@
+import { restar } from "./utils.js";
+
+console.log(restar(10, 5));  // 5
+console.log(restar(20, 7));  // 13
+console.log(restar(3, 8));   // -5
+console.log(restar(100, 25)); // 75
