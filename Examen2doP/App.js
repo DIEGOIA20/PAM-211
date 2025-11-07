2a352d41bd7a7b2499c13531dbe1c75d94e32c6f
DIEGOIA20 <124050360@upq.edu.mx>
2025-11-06 19:51:56 -0600

    Commit1

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, StyleSheet, View, ImageBackground, Dimensions } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hola Mundo en React Native!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const BackgroundImage = require('../assets/Fondo.jpeg');;
+const BackgroundImagen = require('../assets/icon.png');
+
+export default function App({navigation}) {
+    return (
+            <View style={styles.container}>
+                <ImageBackground source={BackgroundImagen} 
+                style={styles.background}
+                resizeMode="cover"> 
+            </ImageBackground>
+                
+        <View style={styles.mainScreen}>
+            <Text style={styles.mainText}>Bienvenido a la pantalla principal</Text>
+        </View>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const { width, height } = Dimensions.get('window');
+    const styles = StyleSheet.create({
+        background: {
+            width: width,
+            height: height,
+        },
+        overlay: {
+            flex: 1,
+            //backgroundColor: 'black',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 20,
+        },
+        title: {
+            fontSize: 32,
+            color: '#fff',
+            fontWeight: 'bold',
+            marginBottom: 10,
+            textAlign: 'center',
+        },
+        mainScreen: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+           // backgroundColor: '#f2f2f2',
+        },
+        mainText: {
+            fontSize: 24,
+            color: '#333',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+    });
